f26a99725550be846740e84b0fb90af79432ab52
malonza-ben <83810291+malonza-ben@users.noreply.github.com>
2023-10-22 04:55:16 +0300

    Update usefulphpcodes.php
    comments io the code

--- usefulphpcodes.php
+++ usefulphpcodes.php
@@ -1,6 +1,7 @@
-<?php
+//this code is used to print hello world as either a browser pdf or save to files in the disk
 
-require('fpdf/fpdf.php');
+<?php
+require('fpdf/fpdf.php');//include the fpdf downloaded from FPDF library from the official website: http://www.fpdf.org/. You can download the latest version from the "Downloads" section.
 $pdf = new FPDF();
 $pdf->AddPage();//Use the AddPage method to add a page to your PDF document.
 $pdf->SetFont('Arial', 'B', 16); //Set Font and Text:
