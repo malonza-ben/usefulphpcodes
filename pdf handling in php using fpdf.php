12d602515bf780ad5262ab4c40235a811f45abdf
malonza-ben <83810291+malonza-ben@users.noreply.github.com>
2023-10-22 21:37:53 +0300

    Update pdf handling in php using fpdf.php
    this is a code and can be edited to any format. it is for displaying pdf to the browser and also to force download. it fetches data from a database stored in the localhost and displays it as a pdf. uses fpdf

--- pdf handling in php using fpdf.php
+++ pdf handling in php using fpdf.php
@@ -11,3 +11,83 @@ $pdf->Output(); //Output to Browser as pdf
 ?>
 
 
+
+
+//Also in the case where i have a database hosted in the local host having the database name as marksprintout and a table student_details
+// the table has the following in it:studentName, registration, Class, maths, english, kiswaili, science, social, tarehe
+//the code can look like the following. and this is just a sample you can edit more 
+<?php
+
+require('fpdf/fpdf.php');//FPDF class in your PHP script using require or include:
+
+$db = new PDO('mysql:host=localhost;dbname=marksprintout', 'root', '');//connecting to the database in the local host 
+
+
+
+//Create an FPDF Object and Add a Page:
+$pdf = new FPDF();//Create an FPDF Object:
+$pdf->AddPage();//Use the AddPage method to add a page to your PDF document.
+
+
+// Set Background Color for the pdf
+$pdf->SetFillColor(200, 200, 200); // Gray background color
+//$pdf->Rect(0, 0, $pdf->w, $pdf->h, 'F'); // Fill the entire page with the background color
+//SetFillColor is used to set the background color.
+//SetTextColor is used to set the text color.
+//SetDrawColor is used to set the border color.
+//SetFont is used to set different fonts and styles for text. 
+
+
+
+//Set the font, font size, and headers for your PDF.
+$pdf->SetFont('Arial', 'B', 11); //Set Font and Text teh B is for making the text bold 
+//headers
+$pdf->Cell(30, 10, 'Student Name', 1);//the 1 is for adding bolders to the table
+$pdf->Cell(25, 10, 'Registration', 1);//the 21, 10 are used to set the cell dimansion on the table displaying
+$pdf->Cell(20, 10, 'Class', 1);
+$pdf->Cell(20, 10, 'Maths', 1);
+$pdf->Cell(20, 10, 'English', 1);
+$pdf->Cell(20, 10, 'Kiswahili', 1);
+$pdf->Cell(20, 10, 'Science', 1);
+$pdf->Cell(20, 10, 'Social', 1);
+$pdf->Cell(20, 10, 'Date', 1);
+$pdf->Ln();// used to enter the next line 
+
+//also you can use the below to display the header 
+// Output the header row
+//$header = array('Student Name', 'Registration', 'Class', 'Maths', 'English', 'Kiswahili', 'Science', 'Social', 'Date');
+//$cellWidth = 40;
+//$cellHeight = 10;
+//foreach ($header as $col) {
+    //$pdf->Cell($cellWidth, $cellHeight, $col, 1, 0, 'C');
+//}
+//$pdf->Ln(); // Move to the next line
+
+// Reset the font to normal for data rows
+$pdf->SetFont('Arial', '', 10);
+$pdf->SetTextColor(0, 0, 0); // set font color for the data rows to black
+
+//Fetch student data from the database and use FPDF's Cell method to output it to the PDF.
+$stmt = $db->query('SELECT * FROM student_details');
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $pdf->Cell(30, 10, $row['studentName'], 1);
+    $pdf->Cell(25, 10, $row['registration'], 1);
+    $pdf->Cell(20, 10, $row['Class'], 1);
+    $pdf->Cell(20, 10, $row['maths'], 1);
+    $pdf->Cell(20, 10, $row['english'], 1);
+    $pdf->Cell(20, 10, $row['kiswaili'], 1);
+    $pdf->Cell(20, 10, $row['science'], 1);
+    $pdf->Cell(20, 10, $row['social'], 1);
+    $pdf->Cell(20, 10, $row['tarehe'], 1);
+    $pdf->Ln();
+}
+
+//$pdf->Output('student_details.pdf', 'D'); // Output and force download
+
+
+$pdf->Output(); //Output to Browser as pdf
+//$pdf->Output('my_pdf.pdf', 'F'); //Save to File
+
+
+?>
+
