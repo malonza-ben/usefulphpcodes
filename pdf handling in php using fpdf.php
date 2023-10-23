a842e0d81a216ad2ede6f7c869b85648507093e4
malonza-ben <83810291+malonza-ben@users.noreply.github.com>
2023-10-23 22:44:53 +0300

    Update pdf handling in php using fpdf.php

--- pdf handling in php using fpdf.php
+++ pdf handling in php using fpdf.php
@@ -5,7 +5,7 @@ require('fpdf/fpdf.php');//include the fpdf downloaded from FPDF library from th
 $pdf = new FPDF();
 $pdf->AddPage();//Use the AddPage method to add a page to your PDF document.
 $pdf->SetFont('Arial', 'B', 16); //Set Font and Text:
-$pdf->Cell(40, 10, 'Hello, World!');
+//$pdf->Cell(40, 10, 'Hello, World!');//to test by displaying hellow world in the pdf
 $pdf->Output(); //Output to Browser as pdf
 //$pdf->Output('my_pdf.pdf', 'F'); //Save to File
 ?>
